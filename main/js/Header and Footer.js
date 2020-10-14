65950fcad6198d0d89a380d785990afe99e19b42
Jakob <jakoblien01@gmail.com>
2020-10-14 10:51:51 +0200

    Fiksa Header og footer modularitet via js, og la inn template på alle sidan som ikkje hadd den

---
+++ main/js/Header and Footer.js
@@ -0,0 +1,14 @@
+document.body.innerHTML = `<header>
+		<a href="index.html"><img id="logo" src="media/images/logo.png" alt="henrik-logo"><h1>Henrik</h1></a>
+		<nav>
+			<a href="index.html">Forside</a><div></div>
+			<a href="etymylogi.html">Etymologi</a><div></div>
+			<a href="bli_en_henrik.html">Bli en Henrik</a><div></div>
+			<a href="historiske_henriker.html">Historiske Henriker</a><div></div>
+			<a href="henrik_or_not.html">Henrik or not?</a><div></div>
+			<a href="sitater.html">Sitater</a>
+		</nav>
+	</header>` + document.body.innerHTML + `<footer>
+		<p>Nettsiden er produsert av Daniel Jan Axlid, Henrik Ytrehus Ågotnes, Thomas Baugerød, Jakob Lien og Morten Tobias Rinde Sunde</p>
+		<!--Lag JS-funksjon som setter footer på bunnen av siden dersom siden er kortere enn skjermehøyden-->
+    </footer>`;

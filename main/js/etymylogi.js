3bc9e98c45ecc92487e905ec0ed3a2dfcd60ca3b
Jakob <jakoblien01@gmail.com>
2020-10-23 13:04:46 +0200

    Fiksa småteri, som å gjør etymologi elementan bold når dem uttales

--- main/js/etymylogi.js
+++ main/js/etymylogi.js
@@ -1,11 +1,15 @@
 window.playing = false;
 function uttal(ord){
+	console.log(ord)
 	let audioElem = document.querySelector("#"+ord);
+	let spanElem = document.querySelector("span[onmouseover=\"uttal('"+ord+"')\"]");
 	if(!window.playing){
+		spanElem.style.fontWeight = "bold";
 		window.playing = true;
 		audioElem.play();
 		setTimeout(function(){ 
 			window.playing = false;
+			spanElem.style.fontWeight = "";
 		}, audioElem.duration*1000);
 	}
 }
